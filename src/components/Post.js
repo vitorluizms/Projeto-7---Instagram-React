43a75e7d706f374be48e0b30f97d55b7e362d1d6
vitorluizms <vitor.luiz.eer@gmail.com>
2023-05-08 15:15:12 -0300

    Funções de salvar e curtir adicionadas (incluindo double click na imagem)

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,8 +1,49 @@
 import { useState } from "react";
 
 export default function Post(props) {
-  
-  
+
+  let [save, setSave] = useState("bookmark-outline")
+  let [like, setLike] = useState("heart-outline")
+  let [classHeart, setClassHeart] = useState('')
+  let [curtidas, setCurtidas] = useState(Number(props.likeNumber).toLocaleString("pt-br"))
+
+  function trocarBookmark() {
+    let saved = "bookmark"
+    if (save === "bookmark-outline") {
+      setSave(saved)
+    }
+    else {
+      setSave("bookmark-outline")
+    }
+  }
+
+  function trocarCurtida() {
+    let liked = "heart"
+    let novaCurtida = Number((props.likeNumber).toLocaleString("pt-br")) + 1
+    let curtidaStr = novaCurtida.toLocaleString("pt-br")
+
+    setLike(liked)
+    setClassHeart('vermelho')
+    setCurtidas(curtidaStr)
+  }
+
+  function trocarLike() {
+    let liked = "heart";
+    let novaCurtida = Number((props.likeNumber).toLocaleString("pt-br")) + 1
+    let curtidaStr = novaCurtida.toLocaleString("pt-br")
+    console.log(curtidaStr)
+    
+    if (like === "heart-outline") {
+      setLike(liked)
+      setClassHeart('vermelho')
+      setCurtidas(curtidaStr)
+    }
+    else{
+      setLike('heart-outline')
+      setClassHeart('')
+      setCurtidas(Number(props.likeNumber).toLocaleString("pt-br"))
+    }
+  }
 
   return (
     <div className="post">
@@ -17,18 +58,18 @@ export default function Post(props) {
       </div>
 
       <div className="conteudo">
-        <img src={props.imagem} alt={props.content} />
+        <img onDoubleClick={trocarCurtida} src={props.imagem} alt={props.content} />
       </div>
 
       <div className="fundo">
         <div className="acoes">
           <div>
-            <ion-icon name="heart-outline"></ion-icon>
+            <ion-icon onClick={trocarLike} class={classHeart} name={like}></ion-icon>
             <ion-icon name="chatbubble-outline"></ion-icon>
             <ion-icon name="paper-plane-outline"></ion-icon>
           </div>
           <div>
-            <ion-icon name="bookmark-outline"></ion-icon>
+            <ion-icon onClick={trocarBookmark} name={save}></ion-icon>
           </div>
         </div>
 
@@ -36,7 +77,7 @@ export default function Post(props) {
           <img src={props.likeSrc} alt={props.likeUser} />
           <div className="texto">
             Curtido por <strong>{props.likeUser}</strong> e
-            <strong> outras {Number(props.likeNumber).toLocaleString("pt-br")} pessoas</strong>
+            <strong> outras {curtidas} pessoas</strong>
           </div>
         </div>
       </div>
